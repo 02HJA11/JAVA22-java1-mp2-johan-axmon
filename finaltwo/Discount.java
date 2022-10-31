7170e85b91672bce2a3e2116451d06be7b535a29
02HJA11 <112394732+02HJA11@users.noreply.github.com>
2022-10-31 22:03:36 +0100

    Update Discount.java

--- finaltwo/Discount.java
+++ finaltwo/Discount.java
@@ -1,11 +1,11 @@
 package finaltwo;
 
 class Discount extends Product{
-		 private double discount = 1.50;
 		 
-		Discount(double discount, String productName, int price, int amountTotal){
+		 
+		Discount(, String productName, double price, int amountTotal){
 			super(productName, price, amountTotal);
-			this.discount = discount;
+			
 			
 		}
 	void discounted(double discount){
